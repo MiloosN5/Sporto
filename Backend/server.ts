6e60c3d5ecddf291c686b38de875cd6978915ef2
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2025-02-26 13:11:29 +0100

    adding PORT

--- Backend/server.ts
+++ Backend/server.ts
@@ -11,9 +11,11 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', authRoutes)
-app.use('/api', contactRoutes)
+app.use('/api', authRoutes);
+app.use('/api', contactRoutes);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
